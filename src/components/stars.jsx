f8135cb608c2a52f42eb446f15fb05715c10b710
DrNickker <mintagirov2015@yandex.ru>
2023-04-03 13:57:59 +0500

    stars_1

--- src/components/stars.jsx
+++ src/components/stars.jsx
@@ -2,6 +2,23 @@ import React from "react"
 import PropTypes from "react"
 import Star from "./star"
 
-export default function Star({count})
+export default function Stars({count}) {
 
 
+    
+    let bool = true
+    if (!Number.isInteger (count) || count > 5 || count < 1) {
+        bool = false
+    }
+    const arr = Array.from({ length: count }, (v, k) => k)
+    return (
+        bool && (
+        <ul className='card-body-stars'>
+            {arr.map((li, id) => <li key={id}>{<Star />}</li>)}
+        </ul>)
+    )
+}
+
+Stars.propTypes = {
+    count: PropTypes.number,
+};
